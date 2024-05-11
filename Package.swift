26f46230a078a844566364deaf94b059d0f338c6
zhanggenning@163.com <zhanggenning@163.com>
2024-05-11 15:08:05 +0800

    Upgrade Swift Packet

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "CocoaLUT",
-            targets: ["CocoaLUT-iOS"])
+            targets: ["CocoaLUT"])
     ],
     dependencies: [
         .package(url: "https://github.com/zhanggenning/Objective-C-RegEx-Categories.git", from: "1.0.0"),
@@ -20,14 +20,13 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "CocoaLUT-iOS",
+            name: "CocoaLUT",
             dependencies: [
                 .product(name: "RegExCategories", package: "Objective-C-RegEx-Categories"),
                 .product(name: "M13OrderedDictionary", package: "M13OrderedDictionary"),
                 .product(name: "XMLDictionary", package: "XMLDictionary")
             ],
             path: "Classes",
-            exclude: ["osx"],
             publicHeadersPath: "."
         )
     ]
